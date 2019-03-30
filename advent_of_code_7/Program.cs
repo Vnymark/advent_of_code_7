996e079bcb1927942c22ec456002db8eb683c2c2
Viktor Nymark <vnymark@gmail.com>
2019-03-30 16:15:09 +0100

    Added Class and function to create Steps from the input.

--- advent_of_code_7/Program.cs
+++ advent_of_code_7/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace advent_of_code_7
 {
@@ -7,6 +10,31 @@ namespace advent_of_code_7
         static void Main(string[] args)
         {
             var inputPath = @"../../../input.txt";
+            List<string> inputText = File.ReadLines(inputPath).ToList();
+            List<Step> AvailableSteps = new List<Step>();
+            foreach (string row in inputText)
+            {
+                AddStep(row);
+                
+            }
+
+            void AddStep(string row) {
+                string stepId = row.Substring(36, 1);
+                string requirement = row.Substring(5, 1);
+                Step step = new Step();
+                step.StepId = stepId;
+                if (AvailableSteps == null || !AvailableSteps.Any(x => (x.StepId == step.StepId)))
+                {
+                    step.Requirements = new List<string>();
+                    AvailableSteps.Add(step);
+                } else
+                {
+                    Step existingStep = AvailableSteps.Find(x => (x.StepId == stepId));
+                    existingStep.Requirements.Add(requirement);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ advent_of_code_7/Step.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace advent_of_code_7
+{
+    class Step
+    {
+        public string StepId { get; set; }
+        public bool Available { get; set; }
+        public bool Completed { get; set; }
+        public List<string> Requirements { get; set; }
+    }
+}
