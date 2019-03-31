7b2b4bd97360d254e8b0d3d6aaa3004375efd513
Viktor Nymark <vnymark@gmail.com>
2019-03-31 20:28:27 +0200

    Added functions for calculating the order of the steps.

--- advent_of_code_7/Program.cs
+++ advent_of_code_7/Program.cs
@@ -11,27 +11,100 @@ namespace advent_of_code_7
         {
             var inputPath = @"../../../input.txt";
             List<string> inputText = File.ReadLines(inputPath).ToList();
+            List<Step> AllSteps = new List<Step>();
             List<Step> AvailableSteps = new List<Step>();
+            List<Step> UnavailableSteps = new List<Step>(AllSteps);
+            string stepLetterOrder = null;
             foreach (string row in inputText)
             {
                 AddStep(row);
                 
             }
+            foreach (string row in inputText)
+            {
+                AddRequirement(row);
+            }
+            
+            GetAvailableSteps();
+            
+
+            while (AvailableSteps.Count > 0)
+            {
+                AvailableSteps.Sort((x, y) => string.Compare(x.Id, y.Id));
+                Step step = AvailableSteps.First();
+                step.Completed = true;
+                UnavailableSteps.Remove(step);
+                AddLetterOrder(step.Id);
+                AvailableSteps.RemoveAt(0);
+                UpdateAvailableSteps(step);
+
+            }
 
             void AddStep(string row) {
-                string stepId = row.Substring(36, 1);
-                string requirement = row.Substring(5, 1);
+                
+                string stepId = row.Substring(5, 1);
+                
                 Step step = new Step();
-                step.StepId = stepId;
-                if (AvailableSteps == null || !AvailableSteps.Any(x => (x.StepId == step.StepId)))
+                step.Id = stepId;
+                if (AllSteps == null || !AllSteps.Any(x => (x.Id == step.Id)))
                 {
-                    step.Requirements = new List<string>();
-                    AvailableSteps.Add(step);
-                } else
+                    AllSteps.Add(step);
+                    UnavailableSteps.Add(step);
+                } 
+            }
+
+            void AddRequirement(string row)
+            {
+                string stepId = row.Substring(36, 1);
+                string requiredStepId = row.Substring(5, 1);
+                if (AllSteps.Any(x => x.Id == stepId))
                 {
-                    Step existingStep = AvailableSteps.Find(x => (x.StepId == stepId));
-                    existingStep.Requirements.Add(requirement);
+                    Step step = AllSteps.Find(x => x.Id == stepId);
+                    Step reqStep = AllSteps.Find(x => x.Id == requiredStepId);
+                    if (step.Requirements == null)
+                    {
+                        step.Requirements = new List<Step>();
+                        step.Requirements.Add(reqStep);
+                    }
+                    else
+                    {
+                        step.Requirements.Add(reqStep);
+                    }
                 }
+            }
+
+            void GetAvailableSteps()
+            {
+                foreach (Step step in AllSteps)
+                {
+                    if (step.Requirements == null && !AvailableSteps.Any(x => x.Id == step.Id))
+                    {
+                        AvailableSteps.Add(step);
+                        UnavailableSteps.Remove(step);
+                    }
+                }
+            }
+
+            void UpdateAvailableSteps(Step completedStep)
+            {
+                foreach (Step step in UnavailableSteps)
+                {
+                    if (step.Requirements.Any(x => x.Id == completedStep.Id))
+                    {
+                        Step reqStep = step.Requirements.Find(x => x.Id == completedStep.Id);
+                        step.Requirements.Remove(reqStep);
+                        
+                    }
+                    if (step.Requirements.Count == 0 && !AvailableSteps.Any(x => x.Id == step.Id))
+                    {
+                        AvailableSteps.Add(step);
+                    }
+                }
+            }
+
+            void AddLetterOrder(string stepId)
+            {
+                stepLetterOrder += stepId;
             }
 
             Console.ReadKey();
--- advent_of_code_7/Step.cs
+++ advent_of_code_7/Step.cs
@@ -6,9 +6,9 @@ namespace advent_of_code_7
 {
     class Step
     {
-        public string StepId { get; set; }
+        public string Id { get; set; }
         public bool Available { get; set; }
         public bool Completed { get; set; }
-        public List<string> Requirements { get; set; }
+        public List<Step> Requirements { get; set; }
     }
 }
