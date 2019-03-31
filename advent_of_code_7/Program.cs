9435f34494e6b3f0adc19b384281617e1275cee1
Viktor Nymark <vnymark@gmail.com>
2019-03-31 20:51:28 +0200

    Working solution to part 1.

--- advent_of_code_7/Program.cs
+++ advent_of_code_7/Program.cs
@@ -10,6 +10,7 @@ namespace advent_of_code_7
         static void Main(string[] args)
         {
             var inputPath = @"../../../input.txt";
+            //var inputPath = @"../../../test.txt";
             List<string> inputText = File.ReadLines(inputPath).ToList();
             List<Step> AllSteps = new List<Step>();
             List<Step> AvailableSteps = new List<Step>();
@@ -26,8 +27,6 @@ namespace advent_of_code_7
             }
             
             GetAvailableSteps();
-            
-
             while (AvailableSteps.Count > 0)
             {
                 AvailableSteps.Sort((x, y) => string.Compare(x.Id, y.Id));
@@ -40,19 +39,28 @@ namespace advent_of_code_7
 
             }
 
+            Console.WriteLine(stepLetterOrder);
+            Console.ReadKey();
+
+            //Go through the row adding both steps if they are missing from the AllSteps list.
             void AddStep(string row) {
-                
-                string stepId = row.Substring(5, 1);
-                
                 Step step = new Step();
-                step.Id = stepId;
+                Step reqStep = new Step();
+                step.Id = row.Substring(36, 1);
+                reqStep.Id = row.Substring(5, 1);
                 if (AllSteps == null || !AllSteps.Any(x => (x.Id == step.Id)))
                 {
                     AllSteps.Add(step);
                     UnavailableSteps.Add(step);
-                } 
+                }
+                if (!AllSteps.Any(x => (x.Id == reqStep.Id)))
+                {
+                    AllSteps.Add(reqStep);
+                    UnavailableSteps.Add(reqStep);
+                }
             }
 
+            //Go through the row and add the the required steps to the different steps.
             void AddRequirement(string row)
             {
                 string stepId = row.Substring(36, 1);
@@ -73,10 +81,12 @@ namespace advent_of_code_7
                 }
             }
 
+            //Get the available steps.
             void GetAvailableSteps()
             {
                 foreach (Step step in AllSteps)
                 {
+                    //Adding the steps to AvailableSteps if they don't already exists.
                     if (step.Requirements == null && !AvailableSteps.Any(x => x.Id == step.Id))
                     {
                         AvailableSteps.Add(step);
@@ -85,6 +95,7 @@ namespace advent_of_code_7
                 }
             }
 
+            //When a step is completed, check what steps that step held back from running and add those to AvailableSteps.
             void UpdateAvailableSteps(Step completedStep)
             {
                 foreach (Step step in UnavailableSteps)
@@ -102,12 +113,11 @@ namespace advent_of_code_7
                 }
             }
 
+            //Add the value of the steps completed to a string.
             void AddLetterOrder(string stepId)
             {
                 stepLetterOrder += stepId;
             }
-
-            Console.ReadKey();
         }
     }
 }
